06b801a57beac0e1c34503bdae455ba995741fc9
Uzeyr OZCAN <muzeyr@gmail.com>
2021-01-03 17:22:31 +0300

    sipariş uygulaması

--- projects/zcn-app/src/app/models/categoryForm.ts
+++ projects/zcn-app/src/app/models/categoryForm.ts
@@ -1,4 +1,5 @@
 import { FormControl, FormGroup, FormArray, Validators, FormBuilder } from '@angular/forms';
+import { CategortDto } from '@modules/kategori/model/category-dto.models';
 import { MusteriDto } from '@modules/musteri/model/musteri';
 import { SiparisDto } from '@modules/siparis/model/siparis';
 import { IModelConvert } from './IModelConvert';
@@ -21,6 +22,23 @@ export class CategroyForm implements IModelConvert{
     
     return new FormGroup(group);
   }
+  convertModelToSiparisFormGroup(model: SiparisDto): FormGroup {
+    const group: any = {};    
+
+    group.id = new FormControl(model.id || '');
+    group.musteriAdi = new FormControl(model.musteriAdi || '');
+    group.urunler =  new FormBuilder().array([])
+    group.siparisUrunler =  new FormBuilder().array([])
+
+    model.kategoriler.forEach(element => {
+      element.products.forEach(item =>{
+        group.urunler.push(this.formUrunEkle(item));
+      })
+    });
+    
+    return new FormGroup(group);
+  }
+  
   convertModelToMusteriFormGroup(model: MusteriDto): FormGroup {
     const group: any = {};    
 
@@ -30,26 +48,22 @@ export class CategroyForm implements IModelConvert{
     group.telefon = new FormControl(model.telefon || '');
     group.aciklama = new FormControl(model.aciklama || '');
     return new FormGroup(group);
-  }
-  convertModelToSiparisFormGroup(model: SiparisDto): FormGroup {
-    const group: any = {};    
-
-    group.id = new FormControl(model.id || '');
-    group.musteriAdi = new FormControl(model.musteriAdi || '');
-    model.kategoriler?.forEach(element => {
-      element.products.forEach(urun =>{
-        group.urunler.push(this.formUrunEkle(urun));
-
-      });
-    });
-
-    return new FormGroup(group);
-  }
-  formUrunEkle(val: any){
+  } 
+  formUrunEkle(val: any){ 
     return new FormBuilder().group({
       id: val.id,
       urunAdi: val.productName,
-      fiyat: val.price
+      fiyat: val.price,
+      adet: val.quantity || ''
+    })
+  }
+  siparisEkle(val: any){ 
+    return new FormBuilder().group({
+      id: val.id,
+      urunAdi: val.urunAdi,
+      fiyat: val.fiyat,
+      adet: val.adet,
+      aciklama: ''
     })
   }
   
--- projects/zcn-app/src/app/modules/siparis/pages/siparis-form/siparis-form.component.ts
+++ projects/zcn-app/src/app/modules/siparis/pages/siparis-form/siparis-form.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CategortDto } from '@modules/kategori/model/category-dto.models';
 import { KategoriService } from '@modules/kategori/service/kategori.service';
+import { MusteriDto } from '@modules/musteri/model/musteri';
 import { MusteriFormDto } from '@modules/musteri/model/musteri-form';
 import { MusteriService } from '@modules/musteri/service/musteri.service';
 import { SiparisDto } from '@modules/siparis/model/siparis';
@@ -9,6 +11,8 @@ import { SiparisService } from '@modules/siparis/service/siparis.service';
 import { CategroyForm } from 'app/models/categoryForm'; 
 import { ToastrManager } from 'ng6-toastr-notifications';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { Observable } from 'rxjs';
+import { startWith,map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-siparis-form',
@@ -18,10 +22,19 @@ import { NgxUiLoaderService } from 'ngx-ui-loader';
 export class  SiparisFormComponent implements OnInit {
  
   siparis: SiparisDto;
+  siparisUrunler: any = [];
   public modelConvert = new CategroyForm();
   public formGroup;
   public musteriler: MusteriFormDto[];
   public kategoriler: CategortDto[];
+  public isLoading: boolean = false;
+  public seciliMusteri: MusteriFormDto;
+  streets: string[] = ['Champs-Élysées', 'Lombard Street', 'Abbey Road', 'Fifth Avenue'];
+
+
+  filteredMusteriler: Observable<MusteriFormDto[]>;
+    control = new FormControl();
+
 
   constructor(private readonly activatedRouter: ActivatedRoute,
     private readonly ngxService: NgxUiLoaderService,
@@ -31,27 +44,36 @@ export class  SiparisFormComponent implements OnInit {
     private readonly kategoriService: KategoriService,
     private readonly musteriService: MusteriService) {
       this.siparis = new SiparisDto();
-      
-
-      this.musteriService.list().subscribe(res=>{
+      this.seciliMusteri = new MusteriFormDto();
+    //  ngxService.start();
+      this.musteriService.list().subscribe(res => {
         this.musteriler = res.data;
       })
-      this.kategoriService.list().subscribe(res=>{
-        this.formGroup = this.modelConvert.convertModelToSiparisFormGroup(this.siparis);
+      this.kategoriService.list().subscribe(res => {
         this.kategoriler = res.data;
+        this.siparis.kategoriler  = res.data;
+        this.formGroup = this.modelConvert.convertModelToSiparisFormGroup(this.siparis);
+
+        ngxService.stop();
+        this.isLoading = true;
+
       })
-    }
+  }
+
   ngOnInit(): void {
+    this.filteredMusteriler = this.control.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+    
     this.activatedRouter.paramMap.subscribe(params => {
       if (params.has('id')) {
         this.ngxService.start();
         this.siparisService.find(params.get('id')).subscribe(data=>{
-          console.log(data);
           this.siparis = data.object;
           this.toastr.successToastr(data.message);
           this.ngxService.stop();
-          this.formGroup = this.modelConvert.convertModelToSiparisFormGroup(this.siparis);
-
+         // this.formGroup = this.modelConvert.convertModelToSiparisFormGroup(this.siparis);
         },error =>{
           this.ngxService.stop();
         })
@@ -60,7 +82,16 @@ export class  SiparisFormComponent implements OnInit {
       }
     });
   } 
+  ekle(item: any){
+    console.log(22);
+    this.siparisUrunler  = this.formGroup.get('siparisUrunler') as FormArray;
+
+    this.siparisUrunler.push(this.modelConvert.siparisEkle(item.value));
+
+    
+  }
   onSubmit(){
+    console.log(this.formGroup.value)
     this.siparisService.save(this.formGroup.value).subscribe(res=>{
       this.toastr.successToastr(res.message);
       this.router.navigateByUrl("/siparis/list");
@@ -68,4 +99,24 @@ export class  SiparisFormComponent implements OnInit {
     });
     
   } 
+  selectionMusteri($event){
+    console.log($event);
+    this.seciliMusteri = $event.value;
+
+  }
+  private _filter(value: string): MusteriFormDto[] {
+    const filterValue = this._normalizeValue(value);
+    return this.musteriler.filter(musteri => this._normalizeValue(musteri.name).includes(filterValue));
+  }
+
+
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
+  }
+  public onMusteriChange(uuid: string): void {
+    this.seciliMusteri =   this.musteriler.find(item=>item.name===uuid)
+
+
+  }
+
 }
